37c18e28779891db632fbb8b28f568837322551b
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-02-17 14:13:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Routes
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Middlewares
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL = 'mongodb://localhost/memories-app';
+const PORT = process.env.PORT || 5000;
+
+// Connected to mongodb
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
