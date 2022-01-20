a0277d8c89313a24b212003c8573e509353f7fcf
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-01-20 15:54:08 +0100

    pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,16 @@ import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
+    const { page } = req.query;
 
-        res.status(200).json(postMessages);
+    try {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+        const total = await PostMessage.countDocuments({});
+
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
     } catch {
         res.status(404).json({ message: error.message });
     }
