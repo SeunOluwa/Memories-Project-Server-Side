898b7bbe289ee60477020d6728a88bb4449c3654
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-01-21 11:28:29 +0100

    single post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPost, getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.get('/search', getPostsBySearch);
+
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
