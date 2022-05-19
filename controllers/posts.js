bf7596c9ce1a343f3cdf6973780e18c3d38f600a
Oluwaseun Taiwo <taiwoluwaseun8@gmail.com>
2022-05-19 02:28:32 -0700

    modified posts controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,7 +12,7 @@ export const getPosts = async (req, res) => {
         const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
 
         res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
-    } catch {
+    } catch (error) {
         res.status(404).json({ message: error.message });
     }
 }
